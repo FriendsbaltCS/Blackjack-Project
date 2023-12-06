4f6204175e9eda5380e31a9fbfb231a01e667d84
jahAtFriends <jhammer@friendsbalt.org>
2023-12-06 11:21:59 -0500

    Add function stubs.

--- script.js
+++ script.js
@@ -26,3 +26,47 @@ function generateDeck(n) {
 
     return deck;
 }
+
+/** 
+ * Determines the value of a given card.
+ * @param {String} card
+ * @returns {number} - The value of the card. Note: Aces are always 11.
+*/
+function evaluateCard(card) {
+    //TODO: Implement this function.
+}
+
+/**
+ * Returns the total value of a given hand.
+ * @param {String[]} hand - An array of cards.
+ * @returns {number} - The total value of the hand.
+ */
+function evaluateHand(hand) {
+    //TODO: Implement this function.
+}
+
+/**
+ * Hits a given hand with a card from the deck.
+ * 
+ * @param {String[]} hand - An array of cards representing a hand.
+ * @param {String[]} deck - The deck to draw the next card from.
+ */
+function hit(hand, deck) {
+    //TODO: Implement this function.
+}
+
+/**
+ * Executes the dealer's entire turn.
+ * 
+ * @param {*} hand - The dealer's hand to play.
+ * @param {*} deck - The deck to play from.
+ */
+function playDealer(hand, deck) {
+    //TODO: Implement this function.
+}
+
+//TODO: Implement the rest of the game and display logic.
+
+let deck = generateDeck(1);
+let playerHand = [];
+let dealerHand = [];
